29fbdd22bd6e11cb11ba126922f7ceaf66dbf4a1
michael-rai <michaelrai.prgmr@gmail.com>
2018-12-03 16:23:54 -0500

    fixing migrations for heroku

--- db/migrate/20181129210526_remove_company_id_from_employee.rb
+++ db/migrate/20181129210526_remove_company_id_from_employee.rb
@@ -1,5 +1,5 @@
 class RemoveCompanyIdFromEmployee < ActiveRecord::Migration[5.2]
   def change
-    remove_column :employees, :company_id, :integer
+    # remove_column :employees, :company_id, :integer
   end
 end
